667753b83d90906fcc97bd10ae14cc1892309054
Varun Ahuja <varun.ahuja@accesscorp.com>
2026-08-17 10:58:14 +0530

    Committing all the changes

---
+++ src/main/java/com/example/Calculator.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class Calculator {
+    public int addMethod(int a,int b){
+        return a+b;
+    }
+}
---
+++ src/test/java/com/example/CalculatorTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+    @Test
+    void addMethod() {
+        Calculator calc = new Calculator();
+        assertEquals(20,calc.addMethod(15,5));
+    }
+}
